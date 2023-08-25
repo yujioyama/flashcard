5ee3938812ea5017a9a4b2b76a4f6d2892be5591
yujioyama <yujioyamanyc@gmail.com>
2023-08-25 19:21:52 +0900

    新しい単語を追加するところまで実装

---
+++ types/word.ts
@@ -0,0 +1,18 @@
+export type Word = {
+  word: string;
+  phonetic: string;
+  phonetics: {
+    text: string;
+    audio: string;
+  }[];
+  origin: string;
+  meanings: {
+    partOfSpeech: string;
+    definitions: {
+      definition: string;
+      example: string;
+      synonyms: string[];
+      antonyms: string[];
+    }[];
+  };
+};
