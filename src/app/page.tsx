6505e49256a8f3cd26a45a4ff0ec80cad5b628b9
大山祐司 <yujioyamanyc@gmail.com>
2023-09-02 16:47:28 +0900

    add eslint, pretter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,89 +1,77 @@
-"use client";
-import styles from "./page.module.scss";
-import { FormEventHandler, useState, use, cache } from "react";
-import axios, { AxiosResponse } from "axios";
-import type { Word } from "../../types/word";
-import clsx from "clsx";
+'use client'
+import axios, { AxiosResponse } from 'axios'
+import clsx from 'clsx'
+import { FormEventHandler, useState, use, cache } from 'react'
+import type { Word } from '../../types/word'
+import styles from './page.module.scss'
 
-const getWords = cache(() =>
-  fetch("http://localhost:3000/api/words").then((res) => res.json())
-);
+const getWords = cache(() => fetch('http://localhost:3000/api/words').then((res) => res.json()))
 
 export default function Home() {
-  const [newWord, setNewWord] = useState<string>("");
-  const [wordList, setWordList] = useState<Word[]>([]);
-  const [isFlipped, setIsFlipped] = useState<boolean>(false);
+  const [newWord, setNewWord] = useState<string>('')
+  const [wordList, setWordList] = useState<Word[]>([])
+  const [isFlipped, setIsFlipped] = useState<boolean>(false)
 
-  const words = use<Word[]>(getWords());
+  const words = use<Word[]>(getWords())
 
-  console.log(words);
+  console.log(words)
 
   const postWord = async (word: Word) => {
-    const res = await fetch("http://localhost:3000/api/words", {
-      method: "POST",
+    const res = await fetch('http://localhost:3000/api/words', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(word),
-    });
+    })
 
-    const json = await res.json();
-  };
+    const json = await res.json()
+  }
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     const response: AxiosResponse<Word[]> = await axios.get(
-      `https://api.dictionaryapi.dev/api/v2/entries/en/${newWord}`
-    );
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${newWord}`,
+    )
 
-    const [word] = response.data;
+    const [word] = response.data
 
-    if (wordList.some((e) => e.word === word.word)) return;
+    if (wordList.some((e) => e.word === word.word)) return
 
-    use(postWord(word));
+    use(postWord(word))
 
-    setWordList([...wordList, word]);
+    setWordList([...wordList, word])
 
-    setNewWord("");
-  };
+    setNewWord('')
+  }
 
   const handlePronunciation = (pronunciation: string) => {
-    const audio = new Audio(pronunciation);
+    const audio = new Audio(pronunciation)
     audio
       .play()
       .then(() => {
-        console.log("Audio started!");
+        console.log('Audio started!')
       })
-      .catch((error) => console.warn(error));
-  };
+      .catch((error) => console.warn(error))
+  }
 
   const handleFlip = () => {
-    setIsFlipped(!isFlipped);
-  };
+    setIsFlipped(!isFlipped)
+  }
 
   return (
     <main className={styles.main}>
       <form onSubmit={handleSubmit}>
         <p>単語・イディオムを追加</p>
-        <input
-          type="text"
-          value={newWord}
-          onChange={(e) => setNewWord(e.target.value)}
-        />
+        <input type='text' value={newWord} onChange={(e) => setNewWord(e.target.value)} />
         <button>追加</button>
       </form>
 
       <ul className={styles.list}>
         {wordList.map((word) => (
           <li key={word.word} className={styles.card} onClick={handleFlip}>
-            <div
-              className={clsx(
-                styles.cardInner,
-                styles.front,
-                isFlipped && styles.isFlipped
-              )}
-            >
+            <div className={clsx(styles.cardInner, styles.front, isFlipped && styles.isFlipped)}>
               <p>{word.word}</p>
               {word.phonetics.map((pronunciation, index) => (
                 <p
@@ -95,25 +83,19 @@ export default function Home() {
               ))}
             </div>
 
-            <div
-              className={clsx(
-                styles.cardInner,
-                styles.back,
-                isFlipped && styles.isFlipped
-              )}
-            >
+            <div className={clsx(styles.cardInner, styles.back, isFlipped && styles.isFlipped)}>
               {word.meanings.map((meaning) => (
                 <div key={meaning.definitions[0].definition}>
                   <p key={meaning.partOfSpeech}>{meaning.partOfSpeech}</p>
                   {meaning.definitions.map((definition, index) => {
-                    const { definition: definitionDesc, example } = definition;
+                    const { definition: definitionDesc, example } = definition
 
                     return (
                       <div key={definitionDesc}>
                         <p key={definitionDesc}>意味：{definitionDesc}</p>
                         {example && <p key={example}>例文：{example}</p>}
                       </div>
-                    );
+                    )
                   })}
                 </div>
               ))}
@@ -122,5 +104,5 @@ export default function Home() {
         ))}
       </ul>
     </main>
-  );
+  )
 }
