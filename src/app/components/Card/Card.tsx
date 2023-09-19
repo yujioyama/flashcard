a29ceeffc4cab3cefe4986047bfd927c4e445ec4
大山祐司 <yujioyamanyc@gmail.com>
2023-09-20 08:20:34 +0900

    テストページ　作成　途中

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -1,7 +1,71 @@
-import React from 'react'
+'use client'
+import clsx from 'clsx'
+import React, { useState } from 'react'
+import type { Word } from '../../types/word'
+import styles from './Card.module.scss'
 
-const Card = () => {
-  return <div>Card</div>
+type Props = {
+  text: string
+}
+
+const Card: React.FC<Props> = ({ text }) => {
+  const [isFlipped, setIsFlipped] = useState<boolean>(false)
+
+  const handlePronunciation = (pronunciation: string) => {
+    const audio = new Audio(pronunciation)
+    audio
+      .play()
+      .then(() => {
+        console.log('Audio started!')
+      })
+      .catch((error) => console.warn(error))
+  }
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped)
+  }
+
+  return (
+    <div className={styles.card} onClick={handleFlip}>
+      <div className={clsx(styles.cardInner, styles.front, isFlipped && styles.isFlipped)}>
+        <p>{text}</p>
+        {/* 
+        <p>{wordSpelling}</p>
+        {phonetics &&
+          phonetics.map(
+            (pronunciation, index) =>
+              pronunciation && (
+                <p
+                  onClick={() => handlePronunciation(pronunciation.audio)}
+                  key={`${pronunciation.audio}_${String(index)}`}
+                >
+                  発音
+                </p>
+              ),
+          )} */}
+      </div>
+
+      <div className={clsx(styles.cardInner, styles.back, isFlipped && styles.isFlipped)}>
+        裏
+        {/* {meanings &&
+          meanings.map((meaning, index) => (
+            <div key={`${meaning.definitions[0].definition}_meanings_${String(index)}`}>
+              <p key={meaning.partOfSpeech}>{meaning.partOfSpeech}</p>
+              {meaning.definitions.map((definition, index) => {
+                const { definition: definitionDesc, example } = definition
+
+                return (
+                  <div key={`${definitionDesc}_definition_${String(index)}`}>
+                    <p>意味：{definitionDesc}</p>
+                    {example && <p key={example}>例文：{example}</p>}
+                  </div>
+                )
+              })}
+            </div>
+          ))} */}
+      </div>
+    </div>
+  )
 }
 
 export default Card
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,14 +1,34 @@
 'use client'
-
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
 import Card from '../components/Card/Card'
 import Main from '../components/Main/Main'
 import MainInner from '../components/MainInner/MainInner'
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 
 const Home = () => {
+  const swiper = useSwiper()
   return (
     <Main>
       <MainInner>
-        <Card />
+        <button onClick={() => swiper.slideNext()}>次</button>
+        <Swiper spaceBetween={50} slidesPerView={1}>
+          <SwiperSlide>
+            <Card text='1' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card text='2' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card text='3' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card text='4' />
+          </SwiperSlide>
+        </Swiper>
       </MainInner>
     </Main>
   )
