5b5459627fdbda771cc886b945f2d31124f326b4
大山祐司 <yujioyamanyc@gmail.com>
2023-09-19 09:50:27 +0900

    working on Test page

---
+++ src/app/test/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import Card from '../components/Card/Card'
+import Main from '../components/Main/Main'
+import MainInner from '../components/MainInner/MainInner'
+
+const Home = () => {
+  return (
+    <Main>
+      <MainInner>
+        <Card />
+      </MainInner>
+    </Main>
+  )
+}
+
+export default Home
