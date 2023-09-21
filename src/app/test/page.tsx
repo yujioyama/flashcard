32e17e788c30931c929e996ff07468db4957767e
yujioyama <you@example.com>
2023-09-21 19:07:00 +0900

    すわいぱー 途中

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,19 +3,22 @@ import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
 import Card from '../components/Card/Card'
 import Main from '../components/Main/Main'
 import MainInner from '../components/MainInner/MainInner'
-// Import Swiper styles
+import styles from './page.module.scss'
 import 'swiper/css'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
-import 'swiper/css/scrollbar'
+
+function SlideNextButton() {
+  const swiper = useSwiper()
+
+  return <button onClick={() => swiper.slideNext()}>Slide to the next slide</button>
+}
 
 const Home = () => {
   const swiper = useSwiper()
   return (
     <Main>
       <MainInner>
-        <button onClick={() => swiper.slideNext()}>次</button>
-        <Swiper spaceBetween={50} slidesPerView={1}>
+        <Swiper slidesPerView={1} className={styles.swiper}>
+          <SlideNextButton />
           <SwiperSlide>
             <Card text='1' />
           </SwiperSlide>
